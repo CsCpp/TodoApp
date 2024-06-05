dfa68420ce608408ec6f54118e546ad4ad5054b6
cscppkotlin@gmail.com <cscppkotlin@gmail.com>
2024-06-05 07:39:21 +0300

    Добавьте файлы проекта.

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<TodoModel> _todoDate;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoDate = new BindingList<TodoModel>() { 
+            new TodoModel(){Text="test"},
+            new TodoModel(){Text="2 not test end"}
+            };
+            dtTodoList.ItemsSource = _todoDate;
+
+            _todoDate.ListChanged += _todoDate_ListChanged;
+
+        }//Window_Loade
+
+        private void _todoDate_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if(e.ListChangedType==ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType== ListChangedType.ItemChanged)
+            {
+                return;
+            }
+          
+        }
+    }
+}
---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+     class TodoModel :INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; }= DateTime.Now;
+        private bool _isDone;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set 
+            { 
+                if (_isDone==value) return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        private string _text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get { return _text; }
+            set 
+            { 
+                if (_text == value) return;
+                _text = value; 
+                OnPropertyChanged("Text");
+            }
+        }
+        protected virtual void OnPropertyChanged(string propertyName="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
